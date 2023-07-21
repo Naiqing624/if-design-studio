720a473ea86f5e39da843c47ffab61cc9463f429
FOURNIER Michael <michael.fournier@ext.europarl.europa.eu>
2023-07-21 19:59:02 +0200

    Send mail

--- src/app/home/contact/contact.component.ts
+++ src/app/home/contact/contact.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { FooterService } from '../services/footer.service';
 
 @Component({
@@ -8,9 +8,21 @@ import { FooterService } from '../services/footer.service';
 })
 export class ContactComponent implements OnInit {
 
+  removeImage = false;
+
   constructor(private readonly footerService: FooterService){}
 
   ngOnInit(): void {
     this.footerService.setCssClass('footerContact');
+    this.checkScreenSize();
+  }
+
+  @HostListener('window:resize', ['$event'])
+  checkScreenSize(): void{
+    if (window.innerWidth < 900) {
+      this.removeImage = true;
+    }else {
+      this.removeImage = false;
+    }
   }
 }
