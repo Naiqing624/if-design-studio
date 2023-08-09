3a207283163665d505c23590484eced8b3d12229
FOURNIER Michael <michael.fournier@ext.europarl.europa.eu>
2023-08-09 11:10:36 +0200

    Finished responsivity

--- src/app/home/book-printing/book-printing.component.ts
+++ src/app/home/book-printing/book-printing.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { LanguageService } from 'src/app/language.service';
 
 @Component({
   selector: 'app-book-printing',
   templateUrl: './book-printing.component.html',
   styleUrls: ['./book-printing.component.scss']
 })
-export class BookPrintingComponent {
+export class BookPrintingComponent implements OnInit{
 
+  selectedLanguage = 'FR';
+
+  constructor(private languageService: LanguageService) {}
+
+  ngOnInit(): void {
+    this.languageService.selectedLanguage$.subscribe(language => {
+      this.selectedLanguage = language;
+    });
+  }
 }
--- src/app/home/ui-ux/ui-ux.component.ts
+++ src/app/home/ui-ux/ui-ux.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
+import { LanguageService } from 'src/app/language.service';
 
 @Component({
   selector: 'app-ui-ux',
   templateUrl: './ui-ux.component.html',
   styleUrls: ['./ui-ux.component.scss']
 })
-export class UiUxComponent {
- 
+export class UiUxComponent implements OnInit{
+  selectedLanguage = 'FR';
+
+  constructor(private languageService: LanguageService) {}
+
+  ngOnInit(): void {
+    this.languageService.selectedLanguage$.subscribe(language => {
+      this.selectedLanguage = language;
+    });
+  }
 }
